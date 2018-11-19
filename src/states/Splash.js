852aae1dae281a65c9bb220762518063fde40b9f
Kathryn <kgrunewald2@bloomberg.net>
2018-11-18 20:42:19 -0500

    shooting bullets

--- src/states/Splash.js
+++ src/states/Splash.js
@@ -15,6 +15,7 @@ export default class extends Phaser.State {
     //
     this.load.image('mushroom', 'assets/images/mushroom2.png')
     this.load.image('preview', 'assets/images/preview.png')
+    this.load.image('bullet', 'assets/images/bullet.png')
     let colors = ['redbar', 'bluebar', 'greenbar']
     for (let c of colors) {
       this.load.spritesheet(c, `assets/images/${c}sheet.png`,
